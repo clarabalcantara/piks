424b743a0e37085db1d9c5707bcd3fe63eefd6c1
oiclai <clarabpnalcantara@gmail.com>
2025-08-15 16:31:38 -0300

    :sparkles: feat: simplify Cliente constructor and add Lancamento class with timestamp and properties

--- src/piks/Cliente.java
+++ src/piks/Cliente.java
@@ -3,9 +3,7 @@ package piks;
 public class Cliente {
     private int cpf; private String nome; private Conta conta;
 
-    public Cliente(int cpf, String nome) {
-        this.cpf = cpf; this.nome = nome; 
-    }
+    public Cliente(int cpf, String nome) { this.cpf = cpf; this.nome = nome; }
 
     public void setConta(Conta cta) { this.conta = cta;}
     public Conta getConta() { return this.conta; }
--- src/piks/Lancamento.java
+++ src/piks/Lancamento.java
@@ -1,5 +1,11 @@
 package piks;
 
-public class Lancamento {
+import java.time.LocalDateTime;
 
+public class Lancamento {
+    private LocalDateTime datahora; private double valor; private Character tipo; // tipo = + ou -
+
+    public Lancamento(double valor, Character tipo) {
+        this.datahora = LocalDateTime.now(); this.valor = valor; this.tipo = tipo; 
+    }
 }
